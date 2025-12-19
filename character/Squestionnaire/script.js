cb05dece8d9a53a4d5ad0c568847d082ba77309a
ChocolatMer <uyuypoi075677@gmail.com>
2025-12-19 18:37:44 +0900

    お

--- character/Squestionnaire/script.js
+++ character/Squestionnaire/script.js
@@ -39,13 +39,14 @@ createApp({
         const currentDate = new Date().toLocaleDateString('ja-JP');
 
         // --- アイテム位置調整 (指示通りに移動 & 画像サイズはCSSで管理) ---
-
+        x: 数字を大きくすると 右 に動きます。
+y: 数字を大きくすると 下 に動きます。
 
         const deskItems = ref([
             { src: '../images/icons/desk1.png', x: 20, y: 400, name: 'Item 1' }, // Down 1 unit
-            { src: '../images/icons/desk2.png', x: 150, y: 250, name: 'Item 2' }, // Up 2, Right 1
-            { src: '../images/icons/desk3.png', x: window.innerWidth - 500, y: 100, name: 'Item 3' }, // Left 2, Down small
-            { src: '../images/icons/desk4.png', x: window.innerWidth - 320, y: 750, name: 'Item 4' }, // Same
+            { src: '../images/icons/desk2.png', x: 200, y: 250, name: 'Item 2' }, // Up 2, Right 1
+            { src: '../images/icons/desk3.png', x: window.innerWidth - 450, y: 100, name: 'Item 3' }, // Left 2, Down small
+            { src: '../images/icons/desk4.png', x: window.innerWidth - 320, y: 850, name: 'Item 4' }, // Same
             { src: '../images/icons/desk5.png', x: 150, y: 550, name: 'Item 5' }, // Down 2, Right small
             { src: '../images/icons/desk6.png', x: window.innerWidth - 350, y: 700, name: 'Item 6' }, // Down 3, Left 2
             { src: '../images/icons/desk7.png', x: 150, y: 850, name: 'Item 7' }, // Down small
