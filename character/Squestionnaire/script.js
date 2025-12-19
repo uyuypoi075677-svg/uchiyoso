36ffa59f9dd60808be3d5bb923b0a6a9cc15c64b
ChocolatMer <uyuypoi075677@gmail.com>
2025-12-19 18:33:11 +0900

    ｒ

--- character/Squestionnaire/script.js
+++ character/Squestionnaire/script.js
@@ -44,10 +44,10 @@ createApp({
         const deskItems = ref([
             { src: '../images/icons/desk1.png', x: 20, y: 400, name: 'Item 1' }, // Down 1 unit
             { src: '../images/icons/desk2.png', x: 150, y: 150, name: 'Item 2' }, // Up 2, Right 1
-            { src: '../images/icons/desk3.png', x: window.innerWidth - 500, y: 50, name: 'Item 3' }, // Left 2, Down small
-            { src: '../images/icons/desk4.png', x: window.innerWidth - 320, y: 650, name: 'Item 4' }, // Same
+            { src: '../images/icons/desk3.png', x: window.innerWidth - 500, y: 100, name: 'Item 3' }, // Left 2, Down small
+            { src: '../images/icons/desk4.png', x: window.innerWidth - 320, y: 750, name: 'Item 4' }, // Same
             { src: '../images/icons/desk5.png', x: 150, y: 550, name: 'Item 5' }, // Down 2, Right small
-            { src: '../images/icons/desk6.png', x: window.innerWidth - 450, y: 700, name: 'Item 6' }, // Down 3, Left 2
+            { src: '../images/icons/desk6.png', x: window.innerWidth - 350, y: 700, name: 'Item 6' }, // Down 3, Left 2
             { src: '../images/icons/desk7.png', x: 150, y: 850, name: 'Item 7' }, // Down small
         ]);
         const dragging = ref({ index: -1, offsetX: 0, offsetY: 0 });
